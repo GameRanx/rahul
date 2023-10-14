c9dbf90b81fae41b4fd660e19c4c8306c9fce183
Rahul Kumar <143000302+rkumar2002@users.noreply.github.com>
2023-10-14 23:06:37 +0530

    Add files via upload

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="section">
+      <h2>Contact Me</h2>
+      <p>Email: rahul.kumar2021a@vitstudetn.ac.in</p>
+      <p>Phone: 9958817275</p>
+      <p>LinkedIn: https://www.linkedin.com/in/rahul-kumar-aa9359219/</p>
+    </div>
+  );
+};
+
+export default Contact;
